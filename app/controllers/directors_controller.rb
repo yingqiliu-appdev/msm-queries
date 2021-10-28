9d9badd98b68fa8013a2985db55c38d506ff1091
yingqi0408 <ying_liu@chicagobooth.edu>
2021-10-28 16:00:29 +0000

    done with eldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,7 +1,16 @@
 class DirectorsController < ApplicationController
 
   def index 
+    @list_of_directors = Director.all
+
     render({ :template => "director_templates/index.html.erb" })
+  end
+
+  def wisest
+
+    @eldest = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+
+    render({ :template => "director_templates/eldest.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
 
   get("/movies", { :controller => "movies", :action => "index"})
 
