875c434781d2333d10c0c127b9a51d36cacf528d
yingqi0408 <ying_liu@chicagobooth.edu>
2021-11-02 19:56:14 +0000

    director details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,20 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/eldest.html.erb"})
   end
 
+  def youngest
+
+    @youngest = Director.where.not({ :dob => nil}).order({ :dob => :desc}).at(0)
+
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+    
+    @the_director = Director.where({ :id => the_id}).at(0)
+
+    render({ :template => "director_templates/show.html.erb"})
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index"})
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+  get("/directors/youngest", { :controller => "directors", :action => "youngest"})
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
 
   get("/movies", { :controller => "movies", :action => "index"})
 
   get("/actors", { :controller => "actors", :action => "index"})
+
+
 
 end
