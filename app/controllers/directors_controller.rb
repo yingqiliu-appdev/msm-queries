abadd472dd809acd8bef5dcd78582f1ef1da4734
yingqi0408 <ying_liu@chicagobooth.edu>
2021-11-02 21:12:09 +0000

    movies done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -24,6 +24,7 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("an_id")
     
     @the_director = Director.where({ :id => the_id}).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id})
 
     render({ :template => "director_templates/show.html.erb"})
   end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,19 @@
 class MoviesController < ApplicationController
 
 def index
+  @list_of_movies = Movie.all
+
   render({ :template => "movie_templates/index.html.erb"})
   
 end
+
+def movie_details
+
+  the_id = params.fetch("an_id")
+
+  @film = Movie.where({ :id => the_id}).at(0)
+  render({ :template => "movie_templates/show.html.erb"})
+
+end 
 
 end
