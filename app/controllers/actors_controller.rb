9acb84d09a6df0cb23a8272bd7204d17895d46b5
yingqi0408 <ying_liu@chicagobooth.edu>
2021-11-03 01:03:13 +0000

    Finished project

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,19 @@
 class ActorsController < ApplicationController
 
   def index
+    @list_of_actors = Actor.all
+
     render({ :template => "actor_templates/index.html.erb"})
+  end
+
+  def actor_details
+
+    the_id = params.fetch("an_id")
+
+    @actor = Actor.where({ :id => the_id}).at(0)
+    @character = Character.where({ :actor_id => @actor.id})
+
+    render({ :template => "actor_templates/show.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get("/movies/:an_id", { :controller => "movies", :action => "movie_details"})
 
   get("/actors", { :controller => "actors", :action => "index"})
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details"})
 
 end
